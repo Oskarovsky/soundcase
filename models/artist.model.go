1b9970b1e5662d0706ebaa8da9719c2fe2b1413e
Oskar Slyk <oskar.slyk@vodeno.com>
2023-08-22 18:53:25 +0200

    Many to many update

--- models/artist.model.go
+++ models/artist.model.go
@@ -1,11 +1,6 @@
 package models
 
-import (
-	"gorm.io/gorm"
-)
-
 type Artist struct {
-	gorm.Model
 	Id          uint     `json:"id" gorm:"primary_key"`
 	Name        string   `json:"name" gorm:"type:varchar(255);not null"`
 	Tracks      []*Track `json:"tracks" gorm:"many2many:artist_track"`
